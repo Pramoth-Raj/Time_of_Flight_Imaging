021e550fd5d1afa27333adc45a038bb53b1d8fb1
Pramoth-Raj <138223784+Pramoth-Raj@users.noreply.github.com>
2024-12-08 00:17:09 +0530

    First commit - added Controller and Scanner

---
+++ Imaging/Controllers.py
@@ -0,0 +1,97 @@
+from serial import Serial
+from TimeTagger import createTimeTagger, Histogram
+import numpy as np
+
+class MotorController:
+
+    def __init__(self, com_port, baud_rate=9600) -> None:
+        self.com_port = com_port
+        self.baud_rate = baud_rate
+        self.ser = Serial(com_port, baudrate=self.baud_rate)
+        self.ser.readline();
+        self.ser.readline();
+        self.ser.readline();
+
+    def send_command(self,command):
+        self.ser.write(command.encode())
+    
+    def home_set(self):
+        self.send_command('X')
+        print(self.ser.readline())
+
+    def right_step(self,dist,speed):
+        self.send_command(f'R{dist} {speed}')
+        print(self.ser.readline())
+
+    def left_step(self,dist,speed):
+        self.send_command(f'L{dist} {speed}')
+        print(self.ser.readline())
+    
+    def down_step(self,dist,speed):
+        self.send_command(f'D{dist} {speed}')
+        print(self.ser.readline())
+
+    def up_step(self,dist,speed):
+        self.send_command(f'U{dist} {speed}')
+        print(self.ser.readline())
+
+    def x_step(self,dist, speed, xdirection):
+        dist*=6400
+        speed*=6400
+        if xdirection == 0:
+            self.left_step(dist,speed)
+        elif xdirection == 1:
+            self.right_step(dist,speed)
+        else:
+            exit()    
+
+    def y_step(self,dist, speed, ydirection):
+        dist*=6400
+        speed*=6400
+        if ydirection == 0:
+            self.up_step(dist,speed)
+        elif ydirection == 1:
+            self.down_step(dist,speed)
+        else:
+            exit()
+
+    def home_step(self):
+        self.send_command('H')  
+        print(self.ser.readline())
+        print(self.ser.readline())
+        print(self.ser.readline())
+
+    def getlocation(self):
+        self.send_command('E')
+        print(self.ser.readline())
+        t =  int(self.ser.readline().split()[-1]), int(self.ser.readline().split()[-1])
+        #print(t)
+        return t[0]/6400,t[1]/6400
+    
+
+class TTController:
+
+    def __init__(self, spad_channel=2, trigger_channel=1, spad_ch_delay=0, trigger_ch_delay=1.072e6):
+        self.spad_channel = spad_channel
+        self.trigger_channel = trigger_channel
+        self.spad_ch_delay = spad_ch_delay
+        self.trigger_ch_delay = trigger_ch_delay
+        self.tagger = createTimeTagger()
+
+    def acquire_histogram(self, acquisition_time=1, bin_width=10e-12, nbins=500):
+        bin_width_ps = bin_width*1e12
+        t=acquisition_time*1e12 #every time data converted to ps
+        hist = Histogram(self.tagger, self.spad_channel, self.trigger_channel, bin_width_ps, nbins)
+        hist.startFor(t) #60e12 #in ps
+        while hist.isRunning():
+            continue    
+        arr = np.array(hist.getData())
+        return arr # returns x in picosecond
+    
+
+class MasterController(MotorController, TTController):
+
+    def __init__(self, com_port, baud_rate=9600, spad_channel=2, trigger_channel=1, spad_ch_delay=0, trigger_ch_delay=1.072e6):
+        MotorController.__init__(self, com_port, baud_rate)
+        TTController.__init__(self, spad_channel, trigger_channel, spad_ch_delay, trigger_ch_delay)
+
---
+++ Imaging/Scanner.py
@@ -0,0 +1,84 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+class Scanner:
+
+    def __init__(self, master_controller) -> None:
+        self.master_controller = master_controller
+
+    def scan_2D(self, delay=0.05, rep_rate=10, acquisition_time=1, bin_width=10e-12, nbins=500, start_direction=(1,0), resolution=(0.1, 0.1), area=(15, 5)):
+        xdirection, ydirection = start_direction
+        total_delay = delay + acquisition_time
+        x_range, y_range = area
+        x_resolution, y_resolution = resolution
+        num_xsteps = int(x_range/x_resolution)
+        num_ysteps = int(y_range/y_resolution)
+        stepper_speed = x_resolution/total_delay
+
+        self.master_controller.home_set()
+
+        start_time = time.time()
+
+        time_delay_2d = []
+        for j in range(num_ysteps):
+            self.master_controller.x_step(x_range,stepper_speed, xdirection)
+            time_array = np.arange(total_delay, total_delay*(num_xsteps+1), total_delay)
+            time_delay_1d = []
+            for i in range(num_xsteps):
+                # if i%100==0:
+                #     print(i)
+                while time.time()<=start_time + time_array[i]:
+                    continue
+                
+                time_delay_1d.append(self.master_controller.acquire_histogram(acquisition_time,bin_width,nbins))
+            time_delay_2d.append(time_delay_1d)
+
+            # while (abs(getlocation()[0]) < x_dist and xdirection) or (getlocation()[0]<0 and not xdirection):
+            #     print(getlocation())
+            #     continue
+            print(j)
+            self.master_controller.y_step(y_resolution,stepper_speed, ydirection)
+
+            while (abs(self.master_controller.getlocation()[1]) < round(y_resolution*(j+1) , 3)):
+                continue
+            xdirection = not(xdirection)
+            
+        self.master_controller.home_step()
+
+        return time_delay_2d
+
+    def scan_1D(self, delay=0.05, rep_rate=10, acquisition_time=1, bin_width=10e-12, nbins=500, start_direction=(1,0), resolution=(0.1, 0.1), area=(15, 5)):
+        xdirection = start_direction[0] if isinstance(start_direction, tuple) else start_direction
+        total_delay = delay + acquisition_time
+        x_range = area[0] if isinstance(area, tuple) else area
+        x_resolution = resolution[0] if isinstance(resolution, tuple) else resolution
+        num_xsteps = int(x_range/x_resolution)
+        stepper_speed = x_resolution/total_delay
+
+        self.master_controller.home_set()
+
+        start_time = time.time()
+
+        self.master_controller.x_step(x_range,stepper_speed, xdirection)
+        time_array = np.arange(total_delay, total_delay*(num_xsteps+1), total_delay)
+        time_delay_1d = []
+
+        for i in range(num_xsteps):
+                # if i%100==0:
+                #     print(i)
+                while time.time()<=start_time + time_array[i]:
+                    continue
+                
+                time_delay_1d.append(self.master_controller.acquire_histogram(acquisition_time,bin_width,nbins))
+
+            # while (abs(getlocation()[0]) < x_dist and xdirection) or (getlocation()[0]<0 and not xdirection):
+            #     print(getlocation())
+            #     continue
+
+        return time_delay_1d
+    
+
+    
+
+
